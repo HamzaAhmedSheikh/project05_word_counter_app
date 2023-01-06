b8c24e8073699739231d0235d5c17de346e72bb7
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-01-06 20:29:15 +0500

    I have styled the word counter app and published it to npm.

--- app.ts
+++ app.ts
@@ -1,12 +1,51 @@
+#!/usr/bin/env node
 import inquirer from 'inquirer';
 import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import gradient from "gradient-string";
+
+let sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    })
+}
+ 
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow(
+        `
+              Welcome to the WORD COUNTER APP \n        
+        `
+    );
+    await sleep();
+    rainbowTitle.stop();
+
+
+    console.log(gradient.pastel.multiline(figlet.textSync("WORD-COUNTER-APP", { horizontalLayout: 'full' })))
+
+
+
+    console.log('\n');
+    console.log(
+        `
+         ${chalk.cyan.bold(`Welcome to our Word Counter App`)}  
+         ${chalk.cyan.bold(`In our word-counter app, you can be counting words`)}   
+         ${chalk.cyan.bold(`and characters just give paragraphs or sentences.`)}    
+                      
+        `
+    );
+}
+
+await welcome()
+
+
 
 let word_counter = async () => {
     let word: { inputString: string } = await inquirer.prompt([
         {
             type: 'input',
             name: 'inputString',
-            message: 'please enter a paragraph or sentence to count the words and characters:',
+            message: `${chalk.green.bold("please enter a paragraph or sentence to count the words and characters:")}`,
             validate(value: string) {
                 return value?.length ? true : "Please add your text";
             },
@@ -42,3 +81,4 @@ async function askAgain() {
 }
 
 await askAgain()
+
